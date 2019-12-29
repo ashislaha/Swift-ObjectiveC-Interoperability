186028a2e614d58cab9026fd5dba044263c94ee3
Ashis Laha <mail2ashislaha@gmail.com>
2019-12-29 18:22:39 +0530

    accessing swift functionlity inside objective c code and accessing objective c functionlity inside swift code which belongs to same module

--- MixLibraryOne/MixLibOneObjcFileA.m
+++ MixLibraryOne/MixLibOneObjcFileA.m
@@ -9,10 +9,19 @@
 #import "MixLibOneObjcFileA.h"
 #import <Foundation/Foundation.h>
 
+/*
+    Let's import the swift functionality inside the same module by importing MixLibOne-Swift.h (SWIFT_OBJC_INTERFACE_HEADER_NAME) file.
+ */
+#import "MixLibOne-Swift.h"
+
 @implementation MixLibOneObjcFileA
 
 -(void) test {
     NSLog(@"test function::: Mix Library One: Objective C fileA");
+    
+    // test swift function
+    MixLibOneSwiftOne *obj = [[MixLibOneSwiftOne alloc] init];
+    [obj test];
 }
 
 @end
--- MixLibraryOne/MixLibOneSwiftOne.swift
+++ MixLibraryOne/MixLibOneSwiftOne.swift
@@ -8,9 +8,19 @@
 
 import Foundation
 
+/*
+    This is not needed because File 'MixLibOneSwiftOne.swift' is part of module 'MixLibraryOne'; ignoring import MixLibraryOne
+    to access objective c functionality.
+*/
+// import MixLibraryOne
+
 @objc open class MixLibOneSwiftOne: NSObject {
     
     @objc open func test() {
         print("test function::: Mix Library One::: Swift One file")
+        
+        
+        let fileA = MixLibOneObjcFileA()
+        fileA.test()
     }
 }
